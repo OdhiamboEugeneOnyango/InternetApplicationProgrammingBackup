a3e766253f9f898fc8fd180b17a39db13cdbbb64
OdhiamboEugeneOnyango <Eugene.Odhiambo@strathmore.edu>
2024-10-02 16:07:33 +0300

    Added divisions and div classes for css stylingfor sign_up_form

--- forms/user_forms.php
+++ forms/user_forms.php
@@ -2,3 +2,7 @@
 class user_forms{
     public function sign_up_form($ObjGlob){
 ?>
+    <div class="row align-items-md-stretch">
+        <div class="col-md-9">
+          <div class="h-100 p-5 text-bg-dark rounded-3">
+            <h2>Sign Up</h2>
