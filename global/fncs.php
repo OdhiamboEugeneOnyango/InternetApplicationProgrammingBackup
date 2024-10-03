efd9c556133b1641a4b45e0a5b382ace0a0d0998
OdhiamboEugeneOnyango <Eugene.Odhiambo@strathmore.edu>
2024-10-03 19:42:14 +0300

    Declared method setMsg  that is a session variable that stores message either in form of an array or a string

--- global/fncs.php
+++ global/fncs.php
@@ -1,2 +1,9 @@
 <?php
 class fncs{
+    public function setMsg($name, $values, $class){
+        if(is_array($values)){
+            $_SESSION[$name] = $values;
+        }else{
+            $_SESSION[$name] = '<span class="'.$class.'">'. $values .'</span>';
+        }
+    }
