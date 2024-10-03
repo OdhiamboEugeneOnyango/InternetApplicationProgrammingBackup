87bcd87b7343c5c43fd489002642695a0ff80255
OdhiamboEugeneOnyango <Eugene.Odhiambo@strathmore.edu>
2024-10-03 19:54:06 +0300

    Declared class and method Send Mail

--- global/SendMail.php
+++ global/SendMail.php
@@ -4,3 +4,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+
+class SendMail{
+
+    //These must be at the top of your script, not inside a function
+    public function SendMail($mailMsg){
+    
