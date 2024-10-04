217a6ab84734e079448c4fb9a0e997afee99dc60
OdhiamboEugeneOnyango <Eugene.Odhiambo@strathmore.edu>
2024-10-04 23:29:28 +0300

    Added error handling for email domain

--- processes/auth.php
+++ processes/auth.php
@@ -24,3 +24,39 @@ class auth{
             if(!filter_var($email_address, FILTER_VALIDATE_EMAIL)){
                 $errors['email_format_err'] = 'Wrong email format';
             }
+
+            // verify that the email domain is authorized (@strathmore.edu, @gmail.com, @yahoo.com, @mada.co.ke) and not (@yanky.net)
+$conf['valid_domains'] = ["strathmore.edu", "gmail.com", "yahoo.com", "mada.co.ke", "outlook.com", "STRATHMORE.EDU", "GMAIL.COM", "YAHOO.COM", "MADA.CO.KE", "OUTLOOK.COM"];
+
+$arr_email_address = explode("@", $email_address);
+$spot_dom = end($arr_email_address);
+$spot_user = reset($arr_email_address);
+
+if(!in_array($spot_dom, $conf['valid_domains'])){
+    $errors['mailDomain_err'] = "Invalid email address domain. Use only: " . implode(", ", $conf['valid_domains']);
+}
+$exist_count = 0;
+// Verify Email Already Exists
+$spot_email_address_res = $conn->count_results(sprintf("SELECT email FROM users WHERE email = '%s' LIMIT 1", $email_address));
+// die($spot_email_address_res);
+if ($spot_email_address_res > $exist_count){
+    $errors['mailExists_err'] = "Email Already Exists";
+}
+
+// verify that the email domain is authorized (@strathmore.edu, @gmail.com, @yahoo.com, @mada.co.ke) and not (@yanky.net)
+$conf['valid_domains'] = ["strathmore.edu", "gmail.com", "yahoo.com", "mada.co.ke", "outlook.com", "STRATHMORE.EDU", "GMAIL.COM", "YAHOO.COM", "MADA.CO.KE", "OUTLOOK.COM"];
+
+$arr_email_address = explode("@", $email_address);
+$spot_dom = end($arr_email_address);
+$spot_user = reset($arr_email_address);
+
+if(!in_array($spot_dom, $conf['valid_domains'])){
+    $errors['mailDomain_err'] = "Invalid email address domain. Use only: " . implode(", ", $conf['valid_domains']);
+}
+$exist_count = 0;
+// Verify Email Already Exists
+$spot_email_address_res = $conn->count_results(sprintf("SELECT email FROM users WHERE email = '%s' LIMIT 1", $email_address));
+// die($spot_email_address_res);
+if ($spot_email_address_res > $exist_count){
+    $errors['mailExists_err'] = "Email Already Exists";
+}
