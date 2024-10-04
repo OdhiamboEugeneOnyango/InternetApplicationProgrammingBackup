848b7a20acb547deba17713a62787c9b00ba503b
OdhiamboEugeneOnyango <Eugene.Odhiambo@strathmore.edu>
2024-10-04 23:21:33 +0300

    Added error handling for email and name format handling

--- processes/auth.php
+++ processes/auth.php
@@ -16,4 +16,11 @@ class auth{
             $email_address = $_SESSION["email_address"] = $conn->escape_values(strtolower($_POST["email_address"]));
             $username = $_SESSION["username"] = $conn->escape_values(strtolower($_POST["username"]));
 
+            if(ctype_alpha(str_replace(" ", "", str_replace("\'", "", $fullname))) === FALSE){
+                $errors['nameLetters_err'] = "Invalid name format: Full name must contain letters and spaces only etc " . $fullname;
+            }
             
+            // verify that the email has got the correct format
+            if(!filter_var($email_address, FILTER_VALIDATE_EMAIL)){
+                $errors['email_format_err'] = 'Wrong email format';
+            }
