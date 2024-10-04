b418060a6d7970f5a5c3aa8fb7562a988c32adc0
OdhiamboEugeneOnyango <Eugene.Odhiambo@strathmore.edu>
2024-10-04 23:04:24 +0300

    Added whitespace

--- processes/auth.php
+++ processes/auth.php
@@ -15,3 +15,5 @@ class auth{
             $fullname = $_SESSION["fullname"] = $conn->escape_values(ucwords(strtolower($_POST["fullname"])));
             $email_address = $_SESSION["email_address"] = $conn->escape_values(strtolower($_POST["email_address"]));
             $username = $_SESSION["username"] = $conn->escape_values(strtolower($_POST["username"]));
+
+            
