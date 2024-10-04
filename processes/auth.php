c2921bc0ed2dc92757288cf19deef3fbcfa92261
OdhiamboEugeneOnyango <Eugene.Odhiambo@strathmore.edu>
2024-10-04 16:18:29 +0300

    Declared new method signup

--- processes/auth.php
+++ processes/auth.php
@@ -6,3 +6,12 @@ class auth{
             return $replacements[$matches[1]];
         }, $template);
     }
+
+    public function signup($conn, $ObjGlob, $ObjSendMail, $lang, $conf){
+        if(isset($_POST["signup"])){
+
+            $errors = array();
+
+            $fullname = $_SESSION["fullname"] = $conn->escape_values(ucwords(strtolower($_POST["fullname"])));
+            $email_address = $_SESSION["email_address"] = $conn->escape_values(strtolower($_POST["email_address"]));
+            $username = $_SESSION["username"] = $conn->escape_values(strtolower($_POST["username"]));
