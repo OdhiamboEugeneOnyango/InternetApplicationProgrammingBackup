0e4a3982f079f686d1c57474035e5183906e6d2e
OdhiamboEugeneOnyango <Eugene.Odhiambo@strathmore.edu>
2024-10-05 10:18:01 +0300

    Added code segment that attemps to connect to MySQL database using PDO

--- pdo.php
+++ pdo.php
@@ -4,3 +4,14 @@
   $username = "root";
   $password = "";
   $dbname = "api_d";
+
+  try {
+    // Create the connection
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully :-)";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+?> 
