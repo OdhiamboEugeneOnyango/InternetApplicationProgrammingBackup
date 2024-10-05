99ec86f5cc0baf409ba99ea1d8a8c4b9d3c91b01
OdhiamboEugeneOnyango <Eugene.Odhiambo@strathmore.edu>
2024-10-05 10:11:31 +0300

    Added code segment to check mysqli conection success

--- misqli.php
+++ misqli.php
@@ -6,3 +6,9 @@
 
     // Create connection
 	$conn = new mysqli($servername, $username, $password, $bdname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+      echo "Connected successfully";
+  ?> 
